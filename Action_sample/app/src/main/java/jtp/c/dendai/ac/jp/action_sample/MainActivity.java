616d56e609a3e986e5ca37b943621d37b707c3cf
suke123 <14ec066@ms.dendai.ac.jp>
2017-07-07 01:48:18 +0900

    Edit

--- Action_sample/app/src/main/java/jtp/c/dendai/ac/jp/action_sample/MainActivity.java
+++ Action_sample/app/src/main/java/jtp/c/dendai/ac/jp/action_sample/MainActivity.java
@@ -5,10 +5,12 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.ImageButton;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     private Droid droid;
+    int lastscore = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,6 +18,11 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         ImageButton startButton = (ImageButton) findViewById(R.id.start_btn);
+        TextView textView = (TextView) findViewById(R.id.scoreText);
+
+
+        textView.setText("LAST GAME ： " + String.valueOf(lastscore));
+
         startButton.setOnClickListener(new android.view.View.OnClickListener() {
             public void onClick(android.view.View v) {
                 // ゲーム画面を起動
